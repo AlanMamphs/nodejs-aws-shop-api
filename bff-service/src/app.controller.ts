bf4b300f7906bb55c3c23b612ac196af32aeffac
Alisher Mamunov <mamunovalisher@gmail.com>
2024-01-08 02:12:58 -0600

    feat: enable caching for get products

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,27 +1,35 @@
-import { Controller, All, Request, Res } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Controller, All, Request, Res, Inject } from '@nestjs/common';
 import axios from 'axios';
+import { Cache } from 'cache-manager';
 import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
   @All('/*')
   async product(@Request() req: Request, @Res() res: Response) {
     const { method, url, body, headers } = req;
-    console.log('HEaders', headers);
-    const headersObject = {};
 
-    // Iterate over request headers and convert to object
-    for (const [key, value] of Object.entries(req.headers)) {
+    // Iterate over request headers and lower case the keys
+    const headersObject = {};
+    for (const [key, value] of Object.entries(headers)) {
       headersObject[key.toLowerCase()] = value;
     }
-    const recepient = url.split('/')[1];
 
+    const recepient = url.split('/')[1];
     const recepientURL = process.env[recepient];
 
     if (recepientURL) {
       const restOfUrl = url.replace(`/${recepient}`, '');
+      const cachedProducts = await this.cacheManager.get('products');
+      console.log(cachedProducts);
+      if (restOfUrl === '/products' && method === 'GET' && cachedProducts) {
+        console.log('Returning cached data');
+        return res.status(200).json(JSON.parse(cachedProducts as string));
+      }
+
       const axiosConfig = {
         method,
         url: `${recepientURL}${restOfUrl}`,
@@ -30,9 +38,15 @@ export class AppController {
         },
         ...(Object.keys(body || {}).length > 0 ? { data: body } : {}),
       };
-      console.log(axiosConfig);
       try {
         const response = await axios(axiosConfig);
+        if (restOfUrl === '/products' && method === 'GET') {
+          console.log('Setting cached data', response.data);
+          await this.cacheManager.set(
+            'products',
+            JSON.stringify(response.data),
+          );
+        }
         return res.status(200).json(response.data);
       } catch (e) {
         if (e.response) {
--- bff-service/src/app.module.ts
+++ bff-service/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      ttl: 120000,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
